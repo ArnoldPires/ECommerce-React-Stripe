542a3ecfc3a1afc7746f0775eaff851e24b4ecbe
ArnoldPires <arnold.pires92@gmail.com>
2023-05-27 13:35:09 -0400

    setting up the dependencies

---
+++ ecommerce/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  // Your component logic goes here
+
+  return (
+    <div>
+      <h1>Welcome to the E-commerce Website</h1>
+      {/* Add your content */}
+    </div>
+  );
+};
+
+export default HomePage;
